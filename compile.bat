d1ccffbb7ba63a3b610736d28f2dbf1098302ca0
Fabrizio Stellato <fabrizio.stellato@infocert.it>
2019-04-08 12:09:32 +0200

    added turrican and metal warrior decoding map with compile.bat

---
+++ compile.bat
@@ -0,0 +1,23 @@
+@echo off
+rem example: compile.bat <file.asm>
+set input=%1
+
+if not defined input goto noinput
+
+set tass64_path=D:\Other\C64\CrossAsm\64tass-1.54.1900
+set vice_path=D:\Other\C64\Emulators\GTK3VICE-3.3-win64-r36165
+set vice_exe=x64sc.exe
+set vice="%vice_path%\%vice_exe%"
+set output=out.prg
+
+set repo="resources"
+
+del %output%
+%tass64_path%\64tass.exe %input% -l labels.txt --tasm-compatible --case-sensitive -I %repo% -o out.prg
+if exist %output% %vice% out.prg 
+
+exit /B 0
+
+:noinput
+echo no 64tass input file specified
+exit /B 1
