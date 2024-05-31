661684f181fb1445e72f815cc9551a7f4d660345
Fabrizio Stellato <fabrizio.stellato@cgm.com>
2024-05-31 11:13:01 +0200

    added stable raster with double irq/sprite tech

--- compile.bat
+++ compile.bat
@@ -2,36 +2,37 @@
 set input=%1
 set crunching=1
 set program_start_address=$5000
+set tass_path=D:\software\64tass-1.56.2625
+set exomizer_path="D:\software\exomizer-3.1.2"
 
 if defined input goto setexec
-set input="D:\Other\C64\Repositories\c64-playground\asm\demo\nightly-city\nightly-city.asm"
+set input="E:\emulators\c64\c64-playground\asm\demo\nightly-city\nightly-city.asm"
 
 :setexec
 set exec=x64sc.exe
 
-set vice_path=D:\Other\C64\Emulators\GTK3VICE-3.5-win64
+set vice_path=E:\emulators\SDL2VICE-3.8-win64
 set vice_exe=%exec%
-set vice="%vice_path%\bin\%vice_exe%"
+set vice="%vice_path%\%vice_exe%"
 set output=out.prg
 set packed_output=packedout.prg
 
-set repo="D:\Other\C64\Repositories\c64-playground\asm\demo\nightly-city\resources"
+set repo="E:\emulators\c64\c64-playground\asm\demo\nightly-city\resources"
 rem options=--tasm-compatible
 
 del %output%
-64tass.exe %input% --vice-labels -l labels.txt %options% --case-sensitive -I %repo% -o out.prg
+%tass_path%\64tass.exe %input% --vice-labels -l labels.txt %options% --case-sensitive -I %repo% -o out.prg
 
-set exomizer_path="D:\Other\C64\Utilities\exomizer-3.0.1"
 
 if %crunching%==1 (
-    %exomizer_path%\win32\exomizer.exe sfx %program_start_address% %output% -o %packed_output%
+    %exomizer_path%\exomizer.exe sfx %program_start_address% %output% -o %packed_output%
     set output=%packed_output%
 )
 
 if exist %output% %vice%  -moncommands labels.txt %output%
 exit /B 0
 
-exomizer.exe sfx $5000 ..\..\..\CrossAsm\64tass-1.55.2200\out.prg -o compressed.prg
+%exomizer_path%\exomizer.exe sfx $5000 %tass_path%\out.prg -o compressed.prg
 
 :noinput
 echo no input file specified
